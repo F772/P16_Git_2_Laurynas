04f953d0fc98d28cb2dae4b270a04ade4e090005
F772 <lauris.gum@gmail.com>
2024-11-28 22:46:38 +0200

    Kodai buvo sugadinti

---
+++ kodas_1.py
@@ -0,0 +1,23 @@
+def gauk_sumas_b(*args, info=False):
+    suma_lyginiu = 0
+    suma_nelyginiu = 0
+    lyginiai = []
+    nelyginiai = []
+    for elem in args:
+        if elem % 2 == 0:
+            suma_lyginiu = elem
+            lyginiai.append(elem)
+        else:
+            suma_nelyginiu = elem
+            nelyginiai.append(elem)
+    if info:
+        print(f"Visi ivesti skaiciai {list(args)}")
+        print(f"skaiciai lyginiai {lyginiai}")
+        print(f"skaiciai nelyginiai {nelyginiai}")
+        print(f"lyginiu skaiciu suma {sum(lyginiai)}")
+        print(f"nelyginiu skaiciu suma {sum(nelyginiai)}")
+    return suma_lyginiu
+
+
+print(type(gauk_sumas_b(1, 1, 2, 2, info=True)))
+print("-------")
---
+++ kodas_2.py
@@ -0,0 +1,13 @@
+def kaupk_aritmetika(*args, operacija="suma"):
+    if len(args) < 2:
+        return args
+    if operacija == "suma":
+        return sum(args)
+    elif operacija == "atimtis":
+        return args[0] - sum(args[1:])
+
+
+print(kaupk_aritmetika(1, 4, 5, operacija="suma"))
+print(kaupk_aritmetika(10, 5, 2, operacija="atimtis"))
+print(kaupk_aritmetika(10, 3, 2, operacija="dalyba"))
+print(kaupk_aritmetika(10, 3, 2, operacija="daugyba"))
